2997d09d2a689390742e11f36fe7ce3db69c9a74
tjrwls0826 <tjrwls0826@naver.com>
2024-02-01 17:01:27 +0900

    Initial commit

---
+++ src/백준/AplusB.java
@@ -0,0 +1,15 @@
+package 백준;
+// https://www.acmicpc.net/problem/1000
+
+
+import java.util.Scanner;
+
+public class AplusB {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a,b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a + b);
+    }
+}
